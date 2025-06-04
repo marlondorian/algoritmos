e057f6c80e0945942d36d311d8cad35aea5bd201
marlondorian <marlondorian35@outlook.com>
2025-06-03 20:51:43 -0600

    funciones con string

--- js/Unidad1/004-introduccion.js
+++ js/Unidad1/004-introduccion.js
@@ -1,6 +1,8 @@
 console.log('Const');
 
-
+// Las variables con const a partir de ES6
+// 1. se utilizan para definir variables que no cambiaran a lo largo del ciclo de vida de la aplicación
+// 2. Las variables con const también tienen 
 function ejemplo() {
     const x = 12;
     if (true) {
---
+++ js/Unidad1/006-funciones.js
@@ -0,0 +1,12 @@
+console.log('String');
+
+const producto = "Monitor 20 pulgadas"
+
+
+console.log(`Total de caracteres ${producto.length}`);
+
+console.log(producto.indexOf('Monitor'));
+console.log(producto.indexOf('i'));
+console.log(producto.indexOf('20'));
+console.log(producto.includes('Monitor'));
+console.log(producto_4);
