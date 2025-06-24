cf8a7ec286db2f3b31519e4fc7683ee662e3bc17
marlondorian <marlondorian35@outlook.com>
2025-06-23 20:07:19 -0600

    arrays

---
+++ js/Unidad1/005-string.js
@@ -0,0 +1,9 @@
+console.log('String');
+
+const producto = "Monitor 20 pulgadas"
+const producto_1 = "Monitor 20 \""
+const producto_2 = 'Monitor 20 "'
+const producto_3 = String('Monitor de 23 "')
+const producto_4 = new String('Monitor de 23 "')
+
+console.log(producto_4);
---
+++ js/Unidad1/007-concatenacion.js
@@ -0,0 +1,10 @@
+const producto = 'bolsa de semitas'
+
+console.log('String');
+const precioNum = 30.00
+const precio = `L ${precioNum}`
+
+//concatenar usando la función concat
+console.log(producto.concat(' ').concat(precio));
+//concatenar usando la función concat
+console.log(producto + ' ' + precio);
